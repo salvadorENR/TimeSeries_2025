a43fd5401a6ca8270894b1c8014c7f62998c7cf9
salvadorENR <salvadorestadistic@gmail.com>
2025-05-18 11:27:06 -0600

    This is the most updated version

--- Session_5.R
+++ Session_5.R
@@ -1,16 +1,17 @@
 # Establecer semilla para reproducibilidad
 set.seed (123)
-# 1. Simular una serie AR (1) con phi = -0.9 y 1000 observaciones
-serie <- arima.sim ( n = 1000, model = list ( ar = 0.95) )
-# 2.Graficar la serie temporal
-plot ( serie , type = "l", main = expression (" Serie AR (1) con "
-                                              * phi == 0.95) ,
-      ylab = expression ( z [t]) , xlab = " Tiempo ")
-# 3.Función de autocorrelación (ACF o FAS)
-acf ( serie , lag.max = 30 , main = "ACF (FAS) de la serie AR (1)
-con = -0.9")
-# 4. Función de autocorrelación parcial (PACF)
-pacf ( serie , lag.max = 30 , main = " PACF de la serie AR (1) con
+# 1. Simular una serie AR(1) con phi = -0.9 y 1000 observaciones
+serie <- arima.sim(n = 1000, model = list(ar = -0.9))
+# 2. Graficar la serie temporal
+plot(serie , type = "l", main = expression("Serie AR(1) con "
+                                           * phi == -0.9),
+     ylab = expression(z[t]), xlab = "Tiempo")
+# 3. F u n c i n de autocorrelaci n (ACF o FAS)
+acf(serie , lag.max = 30, main = "ACF (FAS) de la serie AR(1)
+con
+= -0.9")
+# 4. F u n c i n de autocorrelaci n parcial (PACF)
+pacf(serie , lag.max = 30, main = "PACF de la serie AR(1) con
 = -0.9")
 # 5. Ajustar modelo ARIMA y revisar coeficientes
 modelo<-arima(serie,order=c(1,0,0))
